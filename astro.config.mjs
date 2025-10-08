437d669af9d52f650e97e14477aa8d1d175affd7
Jack Schofield <jackschofield1@googlemail.com>
2025-10-08 09:29:29 +0100

    Revert "update the trust bar"
    This reverts commit 21639964d6a9eb66ce3fef845f966235d8e5fe10.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
+
 import tailwindcss from '@tailwindcss/vite';
+
 import sitemap from '@astrojs/sitemap';
+
 import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: cloudflare(),
   vite: {
     plugins: [tailwindcss()]
   },
-  site: 'https://highqualityclean.co.uk',
+  site: 'https://highqualitycleaning.co.uk',
   integrations: [sitemap(), robotsTxt()]
 });
